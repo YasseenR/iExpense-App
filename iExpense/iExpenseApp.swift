98a033c8b4e74b3da48f534ca456f92f0a56fa9a
YasseenR <yasseenrouni@gmail.com>
2024-09-13 15:57:56 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Yasseen Rouni on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
